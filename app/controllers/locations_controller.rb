ba1d834109abc07adc692b14c1750cc0338c9986
CaseyKelly <casey.kelly@colorado.edu>
2015-02-10 23:54:35 -0700

    Users can view and delete locations on company show page

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,40 @@
+class LocationsController < ApplicationController
+  before_action :set_location, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @location = Location.new
+  end
+
+  def show
+  end
+
+  def index
+    @locations = Location.all
+  end
+
+  def edit
+  end
+
+  def create
+    @location = Location.new(location_params)
+    @company = Company.find(params[:id])
+    redirect_to company_path(@company)
+  end
+
+  def destroy
+    @location.destroy
+    @company = Company.find(params[:company_id])
+    redirect_to company_path(@company)
+  end
+
+  private
+
+  def set_location
+    @location = Location.find(params[:id])
+  end
+
+  def location_params
+    params.require(:location).permit(:city, :street_name, :state, :zip_code, :country)
+  end
+
+end
